bce3a7faf2b08e56913ad383e6e67ebd0a5301bf
Beth <bethhnielsen56@gmail.com>
2020-07-30 19:38:18 -0500

    Add project files.

---
+++ LetterLoop/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data.Common;
+using System.Linq;
+
+namespace LetterLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input some letters");
+
+            var input = Console.ReadLine();
+
+            char[] lettersArr = input.ToCharArray();
+
+            var finalList = lettersArr.Select(c => c.ToString()).ToArray();
+
+            for (int i = 0; i < lettersArr.Length; i++)
+            {
+                var singleChar = string.Join("", Enumerable.Repeat(lettersArr[i], i + 1));
+                finalList[i] = onlyFirstLetterIsUppercased(singleChar);
+            }
+
+            static string onlyFirstLetterIsUppercased(string letter)
+            {
+                if (string.IsNullOrEmpty(letter))
+                {
+                    return string.Empty;
+                }
+                char[] a = letter.ToCharArray();
+                a[0] = char.ToUpper(a[0]);
+                return new string(a);
+
+            }
+            Console.WriteLine(string.Join('-', finalList));
+
+        }
+
+    }
+}
